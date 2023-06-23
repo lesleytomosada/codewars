5f3ce3a5786aa1238743b87af2e5e8215ed2f8b0
Lesley <tomosadalesley@gmail.com>
2023-06-22 23:15:39 -0700

    completed one 6 kyu two 7 kyu

---
+++ 7 kyu/2023-06-22_small_enough?-beginner.py
@@ -0,0 +1,17 @@
+# You will be given an array and a limit value. You must check that all values
+# in the array are below or equal to the limit value. If they are, return
+# true. Else, return false.
+
+# You can assume all values in the array are numbers.
+
+
+def small_enough(array, limit):
+    for x in array:
+        if x > limit:
+            return False
+
+    return True
+
+
+def small_enough2(array, limit):
+    return max(array) <= limit
