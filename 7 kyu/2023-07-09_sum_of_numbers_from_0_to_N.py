e866cc3eba7003dff5fda2bd2b65e0277288ac6c
Lesley <tomosadalesley@gmail.com>
2023-07-09 20:03:06 -0700

    completed one 6 kyu two 7 kyu

---
+++ 7 kyu/2023-07-09_sum_of_numbers_from_0_to_N.py
@@ -0,0 +1,39 @@
+# Description:
+
+# We want to generate a function that computes the series starting from 0 and
+# ending until the given number.
+
+# Example:
+# Input:
+
+# > 6
+# Output:
+
+# 0+1+2+3+4+5+6 = 21
+
+# Input:
+
+# > -15
+# Output:
+
+# -15<0
+
+# Input:
+
+# > 0
+# Output:
+
+# 0=0
+
+
+def show_sequence(n):
+    if n < 0:
+        return f"{n}<0"
+    elif n == 0:
+        return "0=0"
+    res = []
+    total = 0
+    for i in range(n + 1):
+        res.append(str(i))
+        total += i
+    return "+".join(res) + " = " + str(total)
